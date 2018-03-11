a330e0e9094b05d3c23a92d3386ad7c3848c5421
Matthew <mrentz@mail.com>
2018-03-12 08:01:48 +1030

    successfully connectimg to remote EC2 Elasticsearch Server

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
+require 'elasticsearch/model'
+
 class Article < ApplicationRecord
+#class Article < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
+
+Article.import force: true# for auto sync model with elastic search
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   Blog::Application.routes.draw do
     resources :articles
   end
+
+  get 'search', to: 'search#search'
   
 end
